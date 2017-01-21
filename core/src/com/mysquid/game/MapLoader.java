870cb4672b9e333f8d838f0852136ed9e99d60d3
Ferdie <Ferdie@Ferdie-devbox>
2017-01-21 15:14:45 +0000

    Trying to commit maploader again

--- core/src/com/mysquid/game/MapLoader.java
+++ core/src/com/mysquid/game/MapLoader.java
@@ -1,14 +1,7 @@
 package com.mysquid.game;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
-import com.badlogic.gdx.Files;
-import com.badlogic.gdx.Files.FileType;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
 public class MapLoader {
@@ -20,20 +13,11 @@ public char[][] LoadMap(FileHandle MapFile)
 	
 	
 		String data = MapFile.readString();
-		System.out.println(MapFile.readString());
+		//System.out.println(MapFile.readString());
 		String[] dataArr = data.split(",");
 		int row = 0;
 		
-		/*for (int row = 0;row<=23;row++)
-		{
-			for (int col=0;col<=79;col++)
-			{
-				System.out.println(row+" "+col+" "+dataArr[realcol].charAt(0));
-				map[col][row] = dataArr[realcol].charAt(0);
-				realcol++;
-			}
-			
-		}*/
+		
 	BufferedReader in = MapFile.reader(2000);
 	try {
 		while ((data = in.readLine())!=null)
